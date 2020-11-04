1d3573bf382be9f8eaeb3316423a0213416461eb
Rajasegar Chandran <rajasegar.c@gmail.com>
2020-11-04 11:43:24 +0530

    initial commit

---
+++ snowpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  mount: {
+    public: '/',
+    src: '/_dist_',
+  },
+  plugins: [
+    '@snowpack/plugin-svelte',
+    '@snowpack/plugin-dotenv',
+  ],
+  install: [
+    /* ... */
+  ],
+  installOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+  proxy: {
+    /* ... */
+  },
+  alias: {
+    /* ... */
+  },
+};
